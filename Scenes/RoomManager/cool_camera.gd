24d6869a85c2a59be649a5dea82e23e79749dc32
Michael Becker <michael.becker.123@gmail.com>
2024-12-06 20:43:06 -0600

    Finalized Room Transitions
    Hooray

--- Scenes/RoomManager/cool_camera.gd
+++ Scenes/RoomManager/cool_camera.gd
@@ -1,24 +1,27 @@
 extends Camera2D
 class_name CoolCamera
 
-var prevTarget : Vector2 = position;
 var target: Vector2 = position :
 	set(value):
-		prevTarget = target;
 		target = value;
+		targetChanged()
 
-var prevTargetZoom : Vector2 = Vector2(2.0,2.0);
 var targetZoom : Vector2 = Vector2(2.0,2.0) :
 	set(value):
-		prevTargetZoom = targetZoom;
 		targetZoom = value;
+		zoomChanged();
 
-var time = 0.0;
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if position != target || zoom != targetZoom:
-		time = clampf(time + delta * 2.0,0.0,1.0);
-		position = prevTarget.lerp(target,time);
-		zoom = prevTargetZoom.lerp(targetZoom,time);
-	else:
-		time = 0.0;
+@export var transitionSpeed = 0.4;
+
+
+func targetChanged():
+	var tween = get_tree().create_tween();
+	tween.set_trans(tween.TRANS_QUAD);
+	tween.set_ease(Tween.EASE_OUT);
+	tween.tween_property(self,"position",target,transitionSpeed);
+
+func zoomChanged():
+	var tween = get_tree().create_tween();
+	tween.set_trans(tween.TRANS_SINE);
+	tween.set_ease(Tween.EASE_OUT);
+	tween.tween_property(self,"zoom",targetZoom,transitionSpeed);
