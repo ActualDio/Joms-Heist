4176043e05cff55dbd1e61fe33c814b0301d48a9
Michael Becker <michael.becker.123@gmail.com>
2024-12-14 18:48:23 -0600

    camerafix

--- Nodes/Enemies/Camera/camera.gd
+++ Nodes/Enemies/Camera/camera.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 @export var moving = false;
 
+@export_enum("180","135","90","45","0") var angle = 0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if moving:
-		$Main/CameraMotion.play("CameraMotion");
+		match angle:
+			0:
+				$Main/CameraMotion.play("90DegreeArc");
+			1:
+				$Main/CameraMotion.play("default")
+		
+	else:
+		$Main/CameraMotion.play("default");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
